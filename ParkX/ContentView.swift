e0b22661aa6b4706d59c7e0dc15eef97bbadb542
Krishna Teja Mekala <krishna18developer@gmail.com>
2024-03-29 17:58:24 +0530

    Initial Commit

---
+++ ParkX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ParkX
+//
+//  Created by Krishna Teja on 29/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ParkX/ParkXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkXApp.swift
+//  ParkX
+//
+//  Created by Krishna Teja on 29/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ParkXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
